7fb64fb042253a2f3462eac8010d90ea87455610
dazelpro <dazelpro@gmail.com>
2020-10-04 18:48:04 +0700

    form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-local-storage';
+
+    nama = '';
+    jurusan = '';
+    dataSiswa : any = [];
+
+    constructor() {
+        this.getData()
+    }
+
+    getData() {
+        if(localStorage.getItem("data-siswa") !== null){
+            this.dataSiswa = localStorage.getItem("data-siswa");
+        }
+        // localStorage.setItem("data-siswa",JSON.stringify({...JSON.parse(localStorage.getItem("data-siswa")),promo: 'this.couponItem'}));
+    }
+
+    simpanData() {
+        this.dataSiswa.push({nama:this.nama,jurusan:this.jurusan})
+        console.log(this.dataSiswa)
+        // localStorage.setItem("data-siswa",JSON.stringify(this.dataSiswa));
+    }
 }
